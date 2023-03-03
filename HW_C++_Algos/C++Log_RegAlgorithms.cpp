57da91acf462790f3ec8ea4797f1f907f239b323
JiaruiZ <123776546+BuggieZ@users.noreply.github.com>
2023-03-03 17:19:00 -0600

    Commit1

---
+++ HW_C++_Algos/C++Log_RegAlgorithms.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+
+
+int main( int argc, char ** argv)
+{
+
+
+    ifstream inFS;
+    string line;
+    string id_in, pclass_in,survived_in,sex_in, age_in;
+    const int MAX_LEN= 2000;
+
+    vector <double> testSexV (MAX_LEN);
+     vector <double> testSurV (MAX_LEN);
+    vector <double> trainSexV (MAX_LEN);
+    vector <double> trainSurV (MAX_LEN);
+
+
+
+    cout<<"Openning file titanic_project.csv. "<<endl;
+
+    inFS.open("titanic_project.csv");
+    if(!inFS.is_open()) {
+        cout<<" Could not open file."<<endl;
+        return 1;
+    }
+
+
+    cout<<"Reading line 1" <<endl;
+    getline(inFS,line);
+
+    cout<<"heading: "<<line<<endl;
+    int numObservations= 0;
+    int i = 0;
+
+    while(inFS.good()) { 
+
+
+
+        getline(inFS,id_in, ',');
+        getline(inFS,pclass_in, ','); 
+        getline(inFS,survived_in, ',');
+        getline(inFS,sex_in, ','); 
+        getline(inFS,age_in, '\n');
+
+
+      //Take the first 800 to be in test vector
+      if ( numObservations < 800)
+        {
+            trainSurV.at(numObservations) = stof(sex_in);
+            trainSexV.at(numObservations) = stof(sex_in);
+        }
+
+        // take the rest of the observations in as testV
+       if ( (numObservations >= 800) && (numObservations < 1046)&& i <246 )
+        {
+          testsexV.at(i) = stof(sex_in); 
+        testsurV.at(i) = stof(sex_in);
+          i++;
+        }
+
+
+           numObservations++;
+
+        if(numObservations ==1046)
+        {
+            break;
+        } 
+    }
+
+
+    trainV.resize(800);
+    testV.resize(246);
+    cout<< "size for train and test are: " <<endl;
+    cout<<trainV.size()<<endl;
+   cout<<testV.size()<<endl;
+
+
+
+
+    cout<<"Closing file Boston.csv. "<<endl;
+
+
+    cout<<"Number of records: " <<numObservations<<endl;
+
+    cout<<"\nProgram terminaed.";
+
+
+
+    return 0;
+}
